bf7cf3ff4fbedef7a294d1dadd9d2764dc049d42
Punitheprince <puneethrprasad22@gmail.com>
2023-12-28 19:38:10 +0530

    recipe api with serializer only

---
+++ app/recipe/serializers.py
@@ -0,0 +1,14 @@
+"""
+Serilaizers for recipe APIs
+"""
+from rest_framework import serializers
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serialzer for recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
